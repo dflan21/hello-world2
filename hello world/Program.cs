3e020240a58ee1fdb62dccc415057a31c4932a6e
User <User@DESKTOP-TCQ9P99>
2022-08-25 16:31:12 -0400

    Add project files.

---
+++ hello world/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "Hello World!";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("enters user age"); 
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("username is:" + username + "and the age is:" + age); 
+
+        }
+    }
+}
